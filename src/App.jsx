02da445b1a912b0bab9647bee6040c42de5d4d62
Manav Nayak <manav@Manavs-MacBook-Pro.local>
2025-11-20 00:14:11 +0530

    Rearranged components and updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   
   return (
     <>
-    <Auth />
     <Header/>
+    <Auth />
     <Footer/>
 
     </>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(<footer>
-        <p>By continuing, you agree to our <a>Terms</a> & <a>Privacy Policy</a>.</p>
+        <p>By continuing, you agree to our <a href="#">Terms</a> & <a href="#">Privacy Policy</a>.</p>
     </footer>);
 }
 export default Footer;
